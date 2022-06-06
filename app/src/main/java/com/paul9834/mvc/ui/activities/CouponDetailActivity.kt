71ae5d8d774920ce769e6a5c65c2067796758cc1
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2022-06-06 15:40:53 -0500

    implementation CouponDetailActivity

---
+++ app/src/main/java/com/paul9834/mvc/ui/activities/CouponDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.paul9834.mvc.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paul9834.mvc.R
+
+class CouponDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coupon_detail)
+    }
+}
